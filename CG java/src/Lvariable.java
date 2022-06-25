f6a4661fb5f87eaf4d4bd450050dc3b285e745e9
ganes <ganes@DESKTOP-OPBLK4F.bbrouter>
2022-06-25 10:54:22 +0530

    localvariable

---
+++ CG java/src/Lvariable.java
@@ -0,0 +1,18 @@
+
+public class Lvariable {
+
+		public void pupAge() {
+		      int age = 0;
+		      age = age + 7;
+		      System.out.println("Puppy age is : " + age);
+		   }
+
+		   public static void main(String args[]) {
+		      Lvariable test = new Lvariable();
+		      test.pupAge();
+		   }
+		}
+
+
+
+
